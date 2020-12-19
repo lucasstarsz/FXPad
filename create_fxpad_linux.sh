152581344ef0972bc601bb9d334d341d7032d167
lucasstarsz <andrewrcdey@gmail.com>
2020-12-19 02:38:28 -0500

    Fixed double ".txt", updated README, push 0.1.1 HOTFIX.
    - Fixed issue #3: "[BUG] txt showing two times while saving".
    - Added detail & formatting to README file.
      - Increased clarity of instructions for running installer/building project.
      - Formatted parts of README to make intentions clearer.
      - Improved README visual appeal.
    - Replaced arrow lookalikes with alt-keycode arrows in `Edit` → `Indents` → `Shift Left/Right` menu items.
    - Created installers for Windows, Linux.

--- create_fxpad_linux.sh
+++ create_fxpad_linux.sh
@@ -1,9 +1,9 @@
 # Make sure to replace the jmods directory with the location of your javafx jmods.
 # You may also need to add executable permissions to this file. (chmod +x create_fxpad_linux)
-
-JPACKAGE --verbose --name FXPad --app-version 0.1 --description "A clean, simple text editor." \
-	--type msi --linux-shortcut \
-	--module-path "out/artifacts/fxpad_jar/fxpad.jar:F:/Java-JDKs/javafx-jmods-15.0.1" \
+#
+"/opt/jdks/jdk-15/bin/jpackage.exe" --verbose --name FXPad --app-version 0.1.1 \
+	--type deb --linux-shortcut \
+	--module-path "out/artifacts/fxpad_jar/fxpad.jar:../javafx-jmods-15.0.1" \
 	--module fxpad/fxpad.Main \
 	--dest "executable" \
 	--icon "metadata/icons/fxpad_icon.ico" \
